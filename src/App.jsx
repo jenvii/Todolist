918885c1a625b669990c0327e591d56a827b68b7
Jenna <143401323+jenvii@user.noreply.github.com>
2023-10-19 14:39:21 +0300

    MUI tab lisätty

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import './App.css'
-import Todolist from './Todolist'
+import { useState } from "react"
+import "./App.css"
+import Todolist from "./Todolist"
+import Home from "./components/Home";
+import { Tab, Tabs } from "@mui/material";
 
 
 //export default voi laittaa myös funktion eteen :)
 function App() {
+
+  const [value, setValue] = useState('Home');
+
+  const handleChange = (event, value) => {
+    setValue(value);
+  }
+
   return (
-    <Todolist />
+    <>
+      <Tabs value={value} onChange={handleChange}>
+        <Tab value="Home" label="Home"></Tab>
+        <Tab value="Todolist" label="Todos"></Tab>
+      </Tabs>
+      {value === "Home" && <Home />}
+      {value === "Todolist" && <Todolist />}
+    </>
   )
 }
 
 export default App
+
